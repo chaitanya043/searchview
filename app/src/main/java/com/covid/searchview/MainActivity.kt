8a6aabafdb44768ab83ff5f345e42bb662cd2e27
chaitany043 <pgchaitanyaee043@gmail.com>
2021-06-17 21:45:51 +0530

    Searchview

---
+++ app/src/main/java/com/covid/searchview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.covid.searchview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+import java.util.logging.Filter
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+
+}
